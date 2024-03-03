cd29af81a513101cff437e53e49852a69557c7a7
Sam Kee <133723627+samkee10@users.noreply.github.com>
2024-03-03 01:13:11 -0300

    Update isLogin.js

--- backend/middlewares/isLogin.js
+++ backend/middlewares/isLogin.js
@@ -3,21 +3,21 @@ import jwt from "jsonwebtoken"
 
 const isLogin = async (req, res, next) => {
   try {
-    validateHeaders(req, res);
+    validateHeaders(req, res)
     if (req.headers["Authorization"]) {
-      const token = req.headers["Authorization"].split(" ")[1];
-      const decoded = await validateToken(token);
-      req.user = decoded;
-      next();
+      const token = req.headers["Authorization"].split(" ")[1]
+      const decoded = await validateToken(token)
+      req.user = decoded
+      next()
     } else {
-      throw new Error("Authorization header is missing");
+      throw new Error("Authorization header is missing")
     }
   } catch (error) {
-    const errorFound = findError(error.code);
+    const errorFound = findError(error.code)
     if (errorFound && errorFound.length > 0) {
-      return res.status(errorFound[0]?.status || 500).json({ error: errorFound[0]?.message || "Internal Server Error" });
+      return res.status(errorFound[0]?.status || 500).json({ error: errorFound[0]?.message || "Internal Server Error" })
     } else {
-      return res.status(500).json({ error: "Internal Server Error" });
+      return res.status(500).json({ error: "Internal Server Error" })
     }
   }
 }
